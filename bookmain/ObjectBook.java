6e4682e79450fede2c9a408ca485a79ea4887942
HoonEo <eohoon825@gmail.com>
2022-10-22 16:49:41 +0900

    For checking

---
+++ bookmain/ObjectBook.java
@@ -0,0 +1,6 @@
+package bookmain;
+
+public class ObjectBook {
+	public static void main(String[] args) {
+	}
+}
---
+++ theater/Ticket.java
@@ -0,0 +1,15 @@
+package theater;
+
+//import java.time.LocalDateTime;
+
+public class Ticket {
+//	private LocalDateTime when;
+	public String ImAmTicket()
+	{
+		return "Ticket";
+	};
+	
+	public void addFeature()
+	{
+	};
+}
