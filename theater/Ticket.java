f612f7c18ef0e68042de7586d1e47bd52b576259
HoonEo <eohoon825@gmail.com>
2022-10-22 17:11:41 +0900

    Initial Commit

--- theater/Ticket.java
+++ theater/Ticket.java
@@ -1,19 +1,3 @@
 package theater;
 
-//import java.time.LocalDateTime;
 
-public class Ticket {
-//	private LocalDateTime when;
-	public String ImAmTicket()
-	{
-		return "Ticket";
-	};
-	
-	public void addFeature()
-	{
-
-	};
-	public void addNewFeature()
-	{
-	};
-}
